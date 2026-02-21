6ae0439ce897b88ad0210e824d24811717056adc
Lucas Villanueva <lucasvillanueva.dev@gmail.com>
2026-02-21 00:40:52 -0300

    feat: add X-Clacks-Overhead header (GNU Terry Pratchett)
    "A man is not dead while his name is still spoken."

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Clacks-Overhead",
+            value: "GNU Terry Pratchett",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
